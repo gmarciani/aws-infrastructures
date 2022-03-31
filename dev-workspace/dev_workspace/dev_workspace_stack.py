c3e7a33aca6ff4308f75fb9b56be35a3971b75d0
Giacomo Marciani <mgiacomo@amazon.com>
2022-03-31 09:41:43 +0200

    Add stack tags and VPC subnets with regions metadata

--- dev-workspace/dev_workspace/dev_workspace_stack.py
+++ dev-workspace/dev_workspace/dev_workspace_stack.py
@@ -1,4 +1,4 @@
-from aws_cdk import Stack
+from aws_cdk import Stack, Tags
 from constructs import Construct
 from dev_workspace.buckets import SimpleBucket
 from dev_workspace.security_groups import RDPSecurityGroup, SSHSecurityGroup
@@ -9,10 +9,25 @@ class DevWorkspaceStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, config: dict, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
+        # Region
+        region = self.region
+        azs = self.availability_zones
+
+        # Tags
+        Tags.of(self).add("Stack", "DevWorkspaceStack")
+
         # Buckets
         SimpleBucket(self, name=config["BucketName"])
 
         # Networking
-        vpc = SimpleVpc(self, name=config["Vpc"]["Name"], cidr=config["Vpc"]["Cidr"])
+        vpc = SimpleVpc(
+            self,
+            name=config["Vpc"]["Name"],
+            cidr=config["Vpc"]["Cidr"],
+            azs=azs,
+            region_metadata=config["RegionsMetadata"][region],
+        )
+
+        # Security
         SSHSecurityGroup(self, vpc=vpc, prefix_list=config["PrefixLists"][self.region])
         RDPSecurityGroup(self, vpc=vpc, prefix_list=config["PrefixLists"][self.region])
--- dev-workspace/dev_workspace/vpcs.py
+++ dev-workspace/dev_workspace/vpcs.py
@@ -1,9 +1,11 @@
 from aws_cdk import aws_ec2 as ec2
 from constructs import Construct
 
+SUBNET_TYPES = (ec2.SubnetType.PUBLIC, ec2.SubnetType.PRIVATE_ISOLATED)
+
 
 class SimpleVpc(ec2.Vpc):
-    def __init__(self, scope: Construct, name: str, cidr: str):
+    def __init__(self, scope: Construct, name: str, cidr: str, azs: list, region_metadata: dict):
         super().__init__(
             scope=scope,
             id=f"VPC-{name}",
@@ -11,4 +13,11 @@ class SimpleVpc(ec2.Vpc):
             cidr=cidr,
             enable_dns_hostnames=True,
             enable_dns_support=True,
+            subnet_configuration=[
+                ec2.SubnetConfiguration(
+                    name=f"{type.name}-{region_metadata['AvailabilityZones'][az]}".lower(), subnet_type=type
+                )
+                for az in azs
+                for type in SUBNET_TYPES
+            ],
         )
