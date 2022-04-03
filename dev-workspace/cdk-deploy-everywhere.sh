cb0a8b1bb6c36da7ee43a0aa2fa1056d9b8438e2
Giacomo Marciani <mgiacomo@amazon.com>
2022-04-03 20:43:00 +0200

    [DevWorkspace] Forward cdk-deploy-everywhere arguments to the cdk-deploy script

--- dev-workspace/cdk-deploy-everywhere.sh
+++ dev-workspace/cdk-deploy-everywhere.sh
@@ -3,14 +3,17 @@
 # Usage: cdk-deploy-everywhere.sh [ACCOUNT_ID]
 
 ACCOUNT_ID=$1
+shift
 
 [[ -z $ACCOUNT_ID ]] && echo "[ERROR] ACCOUNT_ID is required. Usage: cdk-deploy-everywhere.sh [ACCOUNT_ID]" && exit 1
 
 CURRENT_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 CDK_DEPLOY_SCRIPT="$CURRENT_PATH/cdk-deploy-to.sh"
-REGIONS=$(aws ec2 describe-regions --all-regions --query "Regions[].{Name:RegionName}" --output text)
+REGIONS=$(aws ec2 describe-regions --all-regions --filter Name=opt-in-status,Values=opt-in-not-required --query "Regions[].{Name:RegionName}" --output text)
 
 for region in $REGIONS; do
     echo "Deploying to $ACCOUNT_ID in region $region"
-    bash $CDK_DEPLOY_SCRIPT $ACCOUNT_ID $region
+    check_auth=$(aws sts get-caller-identity --region $region 2>&1)
+    [[ $? -ne 0 ]] && echo "[WARNING] Skipping region $region: cannot authenticate with this region" && continue
+    bash $CDK_DEPLOY_SCRIPT $ACCOUNT_ID $region "$@"
 done
