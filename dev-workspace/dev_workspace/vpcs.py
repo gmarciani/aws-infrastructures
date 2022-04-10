3f308cd4545b699ff4b765a57fbc002f660ef9de
Giacomo Marciani <mgiacomo@amazon.com>
2022-04-10 19:38:56 +0200

    Add name to Subnets

--- dev-workspace/dev_workspace/vpcs.py
+++ dev-workspace/dev_workspace/vpcs.py
@@ -1,3 +1,4 @@
+from aws_cdk import Aspects, Tag
 from aws_cdk import aws_ec2 as ec2
 from common.regions import get_az_name
 from constructs import Construct
@@ -15,10 +16,20 @@ class SimpleVpc(ec2.Vpc):
             enable_dns_hostnames=True,
             enable_dns_support=True,
             subnet_configuration=[
-                ec2.SubnetConfiguration(
-                    name=f"{type.name}-{get_az_name(az, region_metadata)}".lower(), subnet_type=type
-                )
-                for az in azs
-                for type in SUBNET_TYPES
+                ec2.SubnetConfiguration(name=f"{name}/{type}", subnet_type=type) for type in SUBNET_TYPES
             ],
         )
+
+        Aspects.of(self).add(Tag("Name", name))
+        for subnet in self.public_subnets:
+            Aspects.of(subnet).add(
+                Tag("Name", f"{name}/{get_az_name(subnet.availability_zone, region_metadata)}/Public")
+            )
+        for subnet in self.private_subnets:
+            Aspects.of(subnet).add(
+                Tag("Name", f"{name}/{get_az_name(subnet.availability_zone, region_metadata)}/Private")
+            )
+        for subnet in self.isolated_subnets:
+            Aspects.of(subnet).add(
+                Tag("Name", f"{name}/{get_az_name(subnet.availability_zone, region_metadata)}/Isolated")
+            )
