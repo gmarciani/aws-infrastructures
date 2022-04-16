b66a459fb6e23def25683343e9c07b76247fb879
Giacomo Marciani <mgiacomo@amazon.com>
2022-04-16 18:42:15 +0200

    Use private subnets rather than isolated ones. Rename VPC and subnets

--- dev-workspace/dev_workspace/vpcs.py
+++ dev-workspace/dev_workspace/vpcs.py
@@ -3,33 +3,35 @@ from aws_cdk import aws_ec2 as ec2
 from common.regions import get_az_name
 from constructs import Construct
 
-SUBNET_TYPES = (ec2.SubnetType.PUBLIC, ec2.SubnetType.PRIVATE_ISOLATED)
+VPC_NAME = "DevWorkspaceVPC"
+SUBNET_TYPES = (ec2.SubnetType.PUBLIC, ec2.SubnetType.PRIVATE_WITH_NAT)
 
 
 class SimpleVpc(ec2.Vpc):
-    def __init__(self, scope: Construct, name: str, cidr: str, azs: list, region_metadata: dict):
+    def __init__(self, scope: Construct, cidr: str, region_metadata: dict):
         super().__init__(
             scope=scope,
-            id=f"VPC-{name}",
-            vpc_name=name,
+            id=VPC_NAME,
+            vpc_name=VPC_NAME,
             cidr=cidr,
             enable_dns_hostnames=True,
             enable_dns_support=True,
+            max_azs=3,
             subnet_configuration=[
-                ec2.SubnetConfiguration(name=f"{name}/{type}", subnet_type=type) for type in SUBNET_TYPES
+                ec2.SubnetConfiguration(name=f"{VPC_NAME}/{type}", subnet_type=type) for type in SUBNET_TYPES
             ],
         )
 
-        Aspects.of(self).add(Tag("Name", name))
+        Aspects.of(self).add(Tag("Name", VPC_NAME))
         for subnet in self.public_subnets:
             Aspects.of(subnet).add(
-                Tag("Name", f"{name}/{get_az_name(subnet.availability_zone, region_metadata)}/Public")
+                Tag("Name", f"{VPC_NAME}/{get_az_name(subnet.availability_zone, region_metadata)}/Public")
             )
         for subnet in self.private_subnets:
             Aspects.of(subnet).add(
-                Tag("Name", f"{name}/{get_az_name(subnet.availability_zone, region_metadata)}/Private")
+                Tag("Name", f"{VPC_NAME}/{get_az_name(subnet.availability_zone, region_metadata)}/Private")
             )
         for subnet in self.isolated_subnets:
             Aspects.of(subnet).add(
-                Tag("Name", f"{name}/{get_az_name(subnet.availability_zone, region_metadata)}/Isolated")
+                Tag("Name", f"{VPC_NAME}/{get_az_name(subnet.availability_zone, region_metadata)}/Isolated")
             )
