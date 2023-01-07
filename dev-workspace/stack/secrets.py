0595cd57c782bfef70090e931bc509fffda5a637
Giacomo Marciani <mgiacomo@amazon.com>
2023-01-07 16:06:48 +0100

    [Secrets] Using SecretValue.unsafe_plain_text instead of the deprecated SecretStringValueBeta1.from_unsafe_plaintext
    Signed-off-by: Giacomo Marciani <mgiacomo@amazon.com>

--- dev-workspace/stack/secrets.py
+++ dev-workspace/stack/secrets.py
@@ -1,3 +1,4 @@
+from aws_cdk import SecretValue
 from aws_cdk import aws_secretsmanager as secretsmanager
 from constructs import Construct
 
@@ -8,5 +9,5 @@ class SimpleSecret(secretsmanager.Secret):
             scope=scope,
             id=name,
             secret_name=name,
-            secret_string_beta1=secretsmanager.SecretStringValueBeta1.from_unsafe_plaintext(value),
+            secret_string_value=SecretValue.unsafe_plain_text(value),
         )
