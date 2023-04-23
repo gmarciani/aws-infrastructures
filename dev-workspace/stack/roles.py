3696c8616992b9c406c140a4f7ed51095f283ce9
Giacomo Marciani <mgiacomo@amazon.com>
2023-04-23 15:33:49 +0200

    [Roles] Use policy names for AWs managed policies in roles definition.
    Signed-off-by: Giacomo Marciani <mgiacomo@amazon.com>

--- dev-workspace/stack/roles.py
+++ dev-workspace/stack/roles.py
@@ -1,6 +1,9 @@
+from aws_cdk import Fn
 from aws_cdk import aws_iam as iam
 from common.iam_utils import parse_principal
 from constructs import Construct
+
+DEFAULT_PRINCIPAL = "ec2.${AWS::URLSuffix}"
 
 
 class SimpleRole(iam.Role):
@@ -9,21 +12,21 @@ class SimpleRole(iam.Role):
             scope,
             config["Name"],
             role_name=config["Name"],
-            assumed_by=parse_principal(str(config.get("AssumedBy", ["ec2.amazonaws.com"])[0])),
+            assumed_by=parse_principal(str(config.get("AssumedBy", [Fn.sub(DEFAULT_PRINCIPAL)])[0])),
             description=config.get("Description", ""),
         )
 
         for principal_str in config.get("AssumedBy", []):
             self.grant_assume_role(parse_principal(str(principal_str)))
 
-        for policy_arn in config.get("ManagedPolicies", []):
-            super().add_managed_policy(iam.ManagedPolicy.from_aws_managed_policy_name(policy_arn.split("/")[1]))
+        for policy_name in config.get("AwsManagedPolicies", []):
+            super().add_managed_policy(iam.ManagedPolicy.from_aws_managed_policy_name(policy_name))
 
         for policy_statement in config.get("PolicyStatements", []):
             self.add_to_policy(
                 iam.PolicyStatement(
                     actions=policy_statement.get("Actions", []),
-                    resources=policy_statement.get("Resources", []),
+                    resources=[Fn.sub(resource_arn) for resource_arn in policy_statement.get("Resources", [])],
                     conditions=policy_statement.get("Conditions", None),
                 )
             )
