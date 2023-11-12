d271891c9216acb584469c89613515db72504166
Giacomo Marciani <mgiacomo@amazon.com>
2023-11-12 09:43:08 +0100

    [Config] Fix error caused by the use of default dummy AZ when retrieve regions metadata.

--- dev-workspace/common/regions.py
+++ dev-workspace/common/regions.py
@@ -2,6 +2,7 @@ def get_az_name(az: str, region_metadata: dict):
     if (
         region_metadata is None
         or region_metadata["AvailabilityZones"] is None
+        or az.startswith("dummy")
         or region_metadata["AvailabilityZones"][az] is None
     ):
         return az
